4b23a4a11b18336cb70217d50c770fd446f0bbce
veronikarauch <veronika.rauch@yahoo.com>
2015-06-07 16:16:03 +0100

    other Plots code

---
+++ plot2.R
@@ -0,0 +1,16 @@
+setwd("~/Downloads")
+unzip("exdata-data-household_power_consumption.zip", exdir = "Electric_power_consumption")
+Power <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", stringsAsFactors=FALSE)
+PowerTime <- strptime(Power$Time, format="%H:%M:%S")
+PowerTime<-strftime(PowerTime, format="%H:%M:%S")
+PowerDate<-as.Date(Power$Date,format="%d/%m/%Y")
+G1<-data.frame(PowerDate,PowerTime,Power[,3:9])
+Date1<-as.Date("2007-02-01")
+Date2<-as.Date("2007-02-02")
+G2<-G1[G1$PowerDate>=Date1&G1$PowerDate<=Date2,]
+DateTime<-paste(G2$PowerDate,G2$PowerTime)
+DateTime<-as.POSIXct(DateTime, format="%Y-%m-%d %H:%M:%S")
+par(mfrow=c(1, 1))
+plot(DateTime, G2$Global_active_power,type="l",xlab="", ylab="Global Active Power (kilowatts)")
+png(filename = "plot2.png",width = 480, height = 480)
+
---
+++ plot3.R
@@ -0,0 +1,19 @@
+setwd("~/Downloads")
+unzip("exdata-data-household_power_consumption.zip", exdir = "Electric_power_consumption")
+Power <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", stringsAsFactors=FALSE)
+PowerTime <- strptime(Power$Time, format="%H:%M:%S")
+PowerTime<-strftime(PowerTime, format="%H:%M:%S")
+PowerDate<-as.Date(Power$Date,format="%d/%m/%Y")
+G1<-data.frame(PowerDate,PowerTime,Power[,3:9])
+Date1<-as.Date("2007-02-01")
+Date2<-as.Date("2007-02-02")
+G2<-G1[G1$PowerDate>=Date1&G1$PowerDate<=Date2,]
+DateTime<-paste(G2$PowerDate,G2$PowerTime)
+DateTime<-as.POSIXct(DateTime, format="%Y-%m-%d %H:%M:%S")
+plot(DateTime, G2$Sub_metering_1,type="l", xlab="", ylab="Energy sub metering")
+lines(DateTime, G2$Sub_metering_2,type="l", col="red")
+lines(DateTime, G2$Sub_metering_3,type="l", col="blue")
+legend("topright", c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),col=c("black","red","blue"), lty=c(1,1), lwd=c(1,1))
+png(filename = "plot3.png",width = 480, height = 480)
+
+
